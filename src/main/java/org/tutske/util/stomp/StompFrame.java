79ccee51688c9331994d7a6cbb7939da6a777a6a
Groot Sarchy <groot.sarchy@gmail.com>
2017-05-18 16:01:28 +0200

    [ts-1] Add stomp frame implementation and test.

---
+++ src/main/java/org/tutske/util/stomp/StompFrame.java
@@ -0,0 +1,194 @@
+package org.tutske.util.stomp;
+
+import org.tutske.utils.PrimitivesParser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class StompFrame {
+
+	private static final Charset UTF8 = Charset.forName ("utf-8");
+
+	private final byte [] data;
+	private final int start;
+	private final int end;
+	private final int len;
+
+	private String command;
+	private Map<String, String> headers = new HashMap<> ();
+	private byte [] body;
+
+	public static StompFrame fromRaw (byte [] data) {
+		return fromRaw (data, 0, data.length);
+	}
+
+	public static StompFrame fromRaw (byte [] data, int start, int len) {
+		return new StompFrame (data, start, len);
+	}
+
+	public static StompFrame fromData (String command) {
+		return fromData (command, new byte [] {});
+	}
+
+	public static StompFrame fromData (String command, Map<String, Object> headers) {
+		return fromData (command, headers, new byte [] {});
+	}
+
+	public static StompFrame fromData (String command,  byte [] content) {
+		return fromData (command, Collections.EMPTY_MAP, content);
+	}
+
+	public static StompFrame fromData (String command, Map<String, Object> headers, byte [] content) {
+		try ( ByteArrayOutputStream stream = new ByteArrayOutputStream (content.length) ) {
+			stream.write (command.getBytes (UTF8));
+			stream.write ('\n');
+
+			for ( Map.Entry<String, Object> entry : headers.entrySet () ) {
+				stream.write (encode (entry.getKey ()).getBytes (UTF8));
+				stream.write (':');
+				stream.write (encode (entry.getValue ().toString ()).getBytes (UTF8));
+				stream.write ('\n');
+			}
+
+			stream.write ('\n');
+			stream.write (content);
+			stream.write ('\0');
+
+			byte [] raw = stream.toByteArray ();
+
+			return new StompFrame (raw, 0, raw.length);
+		} catch (IOException e) {
+			throw new RuntimeException ("Creating stomp frame failed", e);
+		}
+	}
+
+	private StompFrame (byte [] data, int start, int len) {
+		this.data = data;
+		this.start = start;
+		this.len = len;
+		this.end = start + len;
+
+		parse ();
+	}
+
+	public String command () {
+		return command;
+	}
+
+	public boolean hasHeader (String key) {
+		return headers.containsKey (key);
+	}
+
+	public String header (String key) {
+		return headers.get (key);
+	}
+
+	public <T> T header (String key, Class<T> clazz) {
+		return PrimitivesParser.parse (header (key).trim (), clazz);
+	}
+
+	public Map<String, String> headers () {
+		return headers;
+	}
+
+	public String getBody () {
+		return new String (body, Charset.forName ("utf-8"));
+	}
+
+	public byte [] getByteBody () {
+		return body;
+	}
+
+	private void parse () {
+		int current = start;
+
+		int end = findChar ('\n', current);
+		if ( end < 0 ) { failMessage ("Frame does not start with a command."); }
+
+		command = extract (current, end);
+		current = end + 1;
+
+		while ( true ) {
+			if ( nextCharsAre (current, '\n') || nextCharsAre (current, '\r', '\n') ) {
+				break;
+			}
+
+			int sep = findChar (':', current);
+			if ( sep < 0 ) { failMessage ("Expected a header but no colon found."); }
+
+			int line = findChar ('\n', sep);
+			if ( line < 0 || (sep == current && line > current) ) {
+				failMessage ("header not terminated with a newline");
+			}
+
+			String header = extract (current, sep);
+			String value = extract (sep + 1, line);
+
+			headers.put (decode (header), decode (value));
+			current = line + 1;
+		}
+
+		end = headers.containsKey ("content-length") ?
+			current + 1 + Integer.parseInt (headers.get ("content-length").trim ()) :
+			findChar ('\0', current);
+
+		if ( end > this.end ) {
+			throw new RuntimeException ("content length too large");
+		}
+
+		body = Arrays.copyOfRange (data, current + 1, end);
+	}
+
+	public byte [] raw () {
+		return Arrays.copyOfRange (data, start, end);
+	}
+
+	private boolean nextCharsAre (int index, char ... chars) {
+		for ( int i = 0; i < chars.length; i++ ) {
+			if ( data[i + index] != chars[i] ) { return false; }
+		}
+		return true;
+	}
+
+	private int findChar (char c, int start) {
+		for ( int i = start; i < end; i++ ) {
+			if ( data[i] == c ) { return i; }
+		}
+
+		return -1;
+	}
+
+	private String extract (int start, int end) {
+		if ( data[end - 1 ] == '\r' ) { end = end - 1; }
+		return new String (data, start, end - start);
+	}
+
+	private void failMessage (String reason) {
+		throw new RuntimeException ("Illegal Stomp Message" + (reason.isEmpty () ? "" : ": ") + reason);
+	}
+
+	private static String encode (String value) {
+		return value
+			.replaceAll ("\\\\", "\\\\\\\\")
+			.replaceAll ("\\r", "\\\\r")
+			.replaceAll ("\\n", "\\\\n")
+			.replaceAll (":", "\\\\c")
+			;
+	}
+
+	private static String decode (String value) {
+		return value
+			.replaceAll ("\\\\r", "\r")
+			.replaceAll ("\\\\n", "\n")
+			.replaceAll ("\\\\c", ":")
+			.replaceAll ("\\\\", "\\\\")
+			;
+	}
+
+}
---
+++ src/test/java/org/tutske/util/stomp/StompReparseTest.java
@@ -0,0 +1,62 @@
+package org.tutske.util.stomp;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+
+public class StompReparseTest {
+
+	private StompFrame message = StompFrame.fromData (
+		"COMMAND",
+		Stomp.headers (
+			"content-length", "the body".length (),
+			"with:colon", "colon:colon:colon",
+			"with\\backslach", 1234,
+			"with\nenter", "with\nenter",
+			"with\rcarriage return", "c\rr\r"
+		),
+		"the body".getBytes ()
+	);
+	private StompFrame parsed = StompFrame.fromRaw (message.raw ());
+
+	@Test
+	public void test () {
+		System.out.println (new String (parsed.raw ()));
+	}
+
+	@Test
+	public void it_should_have_the_rigth_command () {
+		assertThat (parsed.command (), is ("COMMAND"));
+	}
+
+	@Test
+	public void it_should_have_the_content_length () {
+		assertThat (parsed.header ("content-length", Integer.class), is (8));
+	}
+
+	@Test
+	public void it_should_have_headers_with_colons () {
+		assertThat (parsed.hasHeader ("with:colon"), is (true));
+		assertThat (parsed.header ("with:colon"), is ("colon:colon:colon"));
+	}
+
+	@Test
+	public void it_should_have_headers_with_enters () {
+		assertThat (parsed.hasHeader ("with\nenter"), is (true));
+		assertThat (parsed.header ("with\nenter"), is ("with\nenter"));
+	}
+
+	@Test
+	public void it_should_have_headers_with_carriage_return () {
+		assertThat (parsed.hasHeader ("with\rcarriage return"), is (true));
+		assertThat (parsed.header ("with\rcarriage return"), is ("c\rr\r"));
+	}
+
+	@Test
+	public void it_should_have_a_body () {
+		assertThat (parsed.getBody (), is ("the body"));
+	}
+
+}
